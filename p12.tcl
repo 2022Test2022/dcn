51f41035911a52081a9ab874a716f422bde932f7
2022Test2022 <124657105+2022Test2022@users.noreply.github.com>
2024-01-10 23:34:16 +0530

    Update p12.tcl

--- p12.tcl
+++ p12.tcl
@@ -1,32 +1,61 @@
 set ns [new Simulator]
-$ns rtproto LS
-set nf [open p7.nam w]
-$ns namtrace-all $nf
+set namfile [open p12.nam w]
+$ns namtrace-all $namfile
+set tracefile [open p12.tr w]
+$ns trace-all $tracefile
 proc finish {} {
-global ns nf
+global ns namfile tracefile
 $ns flush-trace
-close $nf
-exec nam p7.nam &
+close $namfile
+close $tracefile
+exec nam p12.nam &
 exit 0
 }
-for {set i 0} {$i < 7} {incr i} {
-set n($i) [$ns node]
-}
-for {set i 0} {$i < 7} {incr i} {
-$ns duplex-link $n($i) $n([expr ($i+1)%7]) 1Mb 10ms DropTail
-}
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+$ns duplex-link $n0 $n1 1Mb 10ms DropTail
+$ns duplex-link $n0 $n2 1Mb 10ms DropTail
+$ns duplex-link $n0 $n3 1Mb 10ms DropTail
+$ns duplex-link $n1 $n2 1Mb 10ms DropTail
+$ns duplex-link $n1 $n4 1Mb 10ms DropTail
+$ns duplex-link $n2 $n4 1Mb 10ms DropTail
+$ns duplex-link-op $n0 $n1 orient right
+$ns duplex-link-op $n0 $n2 orient right-down
+$ns duplex-link-op $n0 $n3 orient down
+$ns duplex-link-op $n1 $n2 orient left-down
+$ns duplex-link-op $n1 $n4 orient down
+$ns duplex-link-op $n2 $n4 orient right-down
 set udp0 [new Agent/UDP]
-$ns attach-agent $n(0) $udp0
+$ns attach-agent $n0 $udp0
 set cbr0 [new Application/Traffic/CBR]
 $cbr0 set packetSize_ 500
 $cbr0 set interval_ 0.005
 $cbr0 attach-agent $udp0
 set null0 [new Agent/Null]
-$ns attach-agent $n(3) $null0
+$ns attach-agent $n4 $null0
 $ns connect $udp0 $null0
-$ns at 0.5 "$cbr0 start"
-$ns rtmodel-at 1.0 down $n(1) $n(2)
-$ns rtmodel-at 2.0 up $n(1) $n(2)
-$ns at 4.5 " $cbr0 stop"
-$ns at 5.0 "finish "
+set udp1 [new Agent/UDP]
+$ns attach-agent $n2 $udp1
+set cbr1 [new Application/Traffic/CBR]
+$cbr1 set packetSize_ 500
+$cbr1 set interval_ 0.005
+$cbr1 attach-agent $udp1
+set null0 [new Agent/Null]
+$ns attach-agent $n4 $null0
+$ns connect $udp1 $null0
+$ns rtproto LS
+$ns rtmodel-at 20.0 down $n1 $n4
+$ns rtmodel-at 23.0 up $n1 $n4
+$ns rtmodel-at 25.0 down $n2 $n4
+$ns rtmodel-at 40.0 up $n2 $n4
+$udp0 set class_ 1
+$udp1 set class_ 2
+$ns color 1 Red
+$ns color 2 Green
+$ns at 1.0 "$cbr0 start"
+$ns at 2.0 "$cbr1 start"
+$ns at 45 "finish"
 $ns run
